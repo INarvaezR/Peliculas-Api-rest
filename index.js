ec219d12aa9e3d116ca2a8548b6df7eb0eaa5d4b
INarvaezR <isared.narvaez@gmail.com>
2026-03-30 15:45:57 -0500

    fix: add test route and fix port configuration

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const {getConnection} = require("./db/db-connection-mongo");
+const { getConnection } = require("./db/db-connection-mongo");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-/*-----Routes -----*/
+/*----- Ruta de prueba -----*/
+app.get("/test", (req, res) => {
+  res.send("FUNCIONA BACKEND");
+});
+
+/*----- Routes -----*/
 app.use("/api/genero", require("./routes/genero"));
 app.use("/api/media", require("./routes/media"));
 app.use("/api/director", require("./routes/director"));
@@ -21,9 +26,4 @@ getConnection();
 
 app.listen(port, () => {
   console.log("PUERTO USADO:", port);
-});
-
-/*-----Ruta de prueba -----*/
-app.get("/test", (req, res) => {
-  res.send("FUNCIONA BACKEND");
 });
