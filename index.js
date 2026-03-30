fe6c3ef5450f2ed2131c94a2d2293bda2538cad8
INarvaezR <isared.narvaez@gmail.com>
2026-03-30 14:53:12 -0500

    fix: remove duplicate test route and debug port

--- index.js
+++ index.js
@@ -17,17 +17,13 @@ app.use("/api/director", require("./routes/director"));
 app.use("/api/productora", require("./routes/productora"));
 app.use("/api/tipo", require("./routes/tipo"));
 
-/*-----Ruta de prueba -----*/
-app.get("/test", (req, res) => {
-  res.send("FUNCIONA OK");
-});
-
 getConnection();
 
 app.listen(port, () => {
     console.log(`🟢 Servidor corriendo en el puerto ${port}`);
 });
 
+/*-----Ruta de prueba -----*/
 app.get("/test", (req, res) => {
   res.send("FUNCIONA BACKEND");
 });
